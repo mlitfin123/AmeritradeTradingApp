43ba6d2dc53092fffbfb460d4070927b5aae2082
Mark <mark.litfin@yahoo.com>
2020-09-29 14:27:18 -0400

    added looping for multiple stock ordering

--- bars.py
+++ bars.py
@@ -1,11 +1,12 @@
 import config, requests, json
 from datetime import datetime
+import asyncio
 import time
 
 while True:
-    holdings = open ('data/qqq.csv').readlines()
+    holdings = open ('data/movers.csv').readlines()
 
-    symbols = [holding.split(',')[2].strip() for holding in holdings][1:]
+    symbols = [holding.split(',')[0].strip() for holding in holdings]
     symbols = ",".join(symbols)
 
     # oneminute_bars_url = config.BARS_URL + '/1Min?symbols='+ symbols
@@ -15,17 +16,20 @@ while True:
     # r = requests.get(fiveminute_bars_url, headers=config.HEADERS)
     r = requests.get(fiveminute_bars_url, headers=config.HEADERS)
     # r = requests.get(day_bars_url, headers=config.HEADERS)
-
     data = r.json()
+    print(data)
+    async def loop():
+        for symbol in data:
+            print(symbol)
+            filename = 'data/ohlc/{}.txt'.format(symbol)
+            f = open(filename, 'w+')
+            f.write('Date,Open,High,Low,Close,Volume,OpenInterest\n')
+            await asyncio.sleep(.1)
+            for bar in data[symbol]:
+                t = datetime.fromtimestamp(bar['t'])
+                day = t.strftime('%Y-%m-%d-%M')
 
-    for symbol in data:
-        filename = 'data/ohlc/{}.txt'.format(symbol)
-        f = open(filename, 'w+')
-        f.write('Date,Open,High,Low,Close,Volume,OpenInterest\n')
-        for bar in data[symbol]:
-            t = datetime.fromtimestamp(bar['t'])
-            day = t.strftime('%Y-%m-%d-%M')
-
-            line = '{},{},{},{},{},{},{}\n'.format(day, bar['o'], bar['h'], bar['l'], bar['c'], bar['v'], 0.00)
-            f.write(line)
+                line = '{},{},{},{},{},{},{}\n'.format(day, bar['o'], bar['h'], bar['l'], bar['c'], bar['v'], 0.00)
+                f.write(line)
+    asyncio.run(loop())
     time.sleep(60)
--- ta.py
+++ ta.py
@@ -2,106 +2,102 @@ import json, requests, sys
 import btalib
 import pandas as pd
 from datetime import datetime
+import alpaca_trade_api as tradeapi
 from config import ALPACA_KEY, ALPACA_SECRET
 import time
+import logging
+
+in_position = False
 
 while True:
-
-    in_position = False
-    SYMBOL = 'CBAT'
+    holdings = open ('data/movers.csv').readlines()
     BASE_URL = "https://paper-api.alpaca.markets"
-    ACCOUNT_URL = "{}/v2/account".format(BASE_URL)
-    ORDERS_URL = "{}/v2/orders".format(BASE_URL)
-    POSITIONS_URL = "{}/v2/positions/{}".format(BASE_URL, SYMBOL)
+    api = tradeapi.REST(ALPACA_KEY, ALPACA_SECRET, base_url=BASE_URL) # or use ENV Vars shown below
+    account = api.get_account()
+    clock = api.get_clock()
 
-    HEADERS = {'APCA-API-KEY-ID': ALPACA_KEY, 'APCA-API-SECRET-KEY': ALPACA_SECRET}
+    for symbol in holdings:
+        SYMBOL = symbol.rstrip("\n")
+        print(SYMBOL)
 
-    def place_order():
-        data = {
-            "symbol": SYMBOL,
-            "qty": 50,
-            "side": "buy",
-            "type": "market",
-            "time_in_force": "gtc",
-        }
+        def place_order():
+            api.submit_order(
+                symbol=SYMBOL,
+                qty="50",
+                side="buy",
+                type="market",
+                time_in_force="gtc",
+            )
 
-        r = requests.post(ORDERS_URL, json=data, headers=HEADERS)
-
-        response = json.loads(r.content)
-
-        print(response)
-
-    def close_position():
-        data = {
-            "symbol": SYMBOL,
-            "qty": 50,
-            "side": "sell",
-            "type": "market",
-            "time_in_force": "gtc",
-        }
-
-        r = requests.post(ORDERS_URL, json=data, headers=HEADERS)
-
-        response = json.loads(r.content)
-
-        print(response)
-
-    # Read a csv file into a pandas dataframe
-    df = pd.read_csv('data/ohlc/CBAT.txt', parse_dates=True, index_col='Date')
-    sma = btalib.sma(df)
-
-    sma = btalib.sma(df)
-    rsi = btalib.rsi(df)
-
-    df['sma'] = sma.df
-    df['rsi'] = rsi.df
-
-    macd = btalib.macd(df)
-
-    df['macd'] = macd.df['macd']
-    df['signal'] = macd.df['signal']
-    df['histogram'] = macd.df['histogram']
-    print(df)
-    df.rsi.to_csv('data/technicals/rsi.csv', header=True, index=False)
-    df.macd.to_csv('data/technicals/macd.csv', header=True, index=False)
-    df.signal.to_csv('data/technicals/signal.csv', header=True, index=False)
-
-    readRSI = open ('data/technicals/rsi.csv').readlines()
-    readMACD = open ('data/technicals/macd.csv').readlines()
-    readSignal = open ('data/technicals/signal.csv').readlines()
-
-    newRSI= float(readRSI[100].rstrip("\n"))
-    newMACD= float(readMACD[100].rstrip("\n"))
-    newSignal= float(readSignal[100].rstrip("\n"))
-    print(newRSI)
-    print(newMACD)
-    print(newSignal)
-    if newRSI < 30:
-        print('Oversold!')
-        if not in_position:
-            print("== Placing order and setting in position to true ==")
+        def close_position():
+            print(SYMBOL)
+            api.close_position(
+                symbol=SYMBOL
+            )
+            
+        try:
+            api.get_position(SYMBOL)
             in_position = True
-            place_order()
-
-    if newRSI > 70:
-        print('Overbought!')
-        if in_position:
-            print("== Closing position and setting in position to false ==")
+            print (in_position)
+        except Exception as e:
+            logging.error(e)
             in_position = False
-            close_position()
+            print (in_position)
 
-    if newMACD > newSignal:
-        print('MACD Buy Signal!')
-        if not in_position:
-            print("== Placing order and setting in position to true ==")
-            in_position = True
-            place_order()
-    
-    if newMACD < newSignal:
-        print('MACD Sell Signal!')
-        if in_position:
-            print("== Closing position and setting in position to false ==")
-            in_position = False
-            place_order()
+        # Read a csv file into a pandas dataframe
+        df = pd.read_csv('data/ohlc/'+ SYMBOL +'.txt', parse_dates=True, index_col='Date')
+        sma = btalib.sma(df)
+
+        sma = btalib.sma(df)
+        rsi = btalib.rsi(df)
+
+        df['sma'] = sma.df
+        df['rsi'] = rsi.df
+
+        macd = btalib.macd(df)
+
+        df['macd'] = macd.df['macd']
+        df['signal'] = macd.df['signal']
+        df['histogram'] = macd.df['histogram']
+        print(df)
+        df.rsi.to_csv('data/technicals/'+ SYMBOL +'rsi.csv', header=True, index=False)
+        df.macd.to_csv('data/technicals/'+ SYMBOL +'macd.csv', header=True, index=False)
+        df.signal.to_csv('data/technicals/'+ SYMBOL +'signal.csv', header=True, index=False)
+
+        readRSI = open ('data/technicals/'+ SYMBOL +'rsi.csv').readlines()
+        readMACD = open ('data/technicals/'+ SYMBOL +'macd.csv').readlines()
+        readSignal = open ('data/technicals/'+ SYMBOL +'signal.csv').readlines()
+
+        newRSI= float(readRSI[100].rstrip("\n"))
+        newMACD= float(readMACD[100].rstrip("\n"))
+        newSignal= float(readSignal[100].rstrip("\n"))
+        print(newRSI)
+        print(newMACD)
+        print(newSignal)
+
+        print('The market is {}'.format('open.' if clock.is_open else 'closed.'))
+        if clock.is_open and newRSI < 30:
+            print('Oversold!')
+            if in_position == False:
+                print("== Placing order ==")
+                place_order()
+
+        if clock.is_open and newRSI > 70:
+            print('Overbought!')
+            if in_position == True:
+                print("== Closing position ==")
+                close_position()
+
+        if clock.is_open and newMACD > newSignal:
+            print('MACD Buy Signal!')
+            if in_position == False and newRSI < 60:
+                print("== Placing order ==")
+                place_order()
+        
+        if newMACD < newSignal:
+            print('MACD Sell Signal!')
+            if in_position == True and newRSI > 30:
+                print("== Closing position ==")
+                close_position()
 
     time.sleep(60)
